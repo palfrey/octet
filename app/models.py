ce25a3c4e9d42407683a756fb3ffa8f40e8cd8be
Tom Parker-Shemilt <palfrey@tevp.net>
2023-05-30 16:52:29 +0100

    Data download

--- app/models.py
+++ app/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+class Organiser(models.Model):
+    name = models.TextField(unique=True)
+
+class Event(models.Model):
+    name = models.TextField(unique=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    organiser = models.ForeignKey(Organiser, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [models.UniqueConstraint(name="event_organiser_name",
+    fields=["organiser", "name"],
+)]
+
+class Contest(models.Model):
+    name = models.TextField()
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [models.UniqueConstraint(name="contest_event_name",
+    fields=["event", "name"],
+)]
+
+class Group(models.Model):
+    name = models.TextField(unique=True)
+    director = models.TextField()
+    member_count = models.IntegerField()
+
+class Performance(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    contest = models.ForeignKey(Contest, on_delete=models.CASCADE)
+    song_name = models.TextField()
+    singing_score = models.IntegerField()
+    performance_score = models.IntegerField()
+    music_score = models.IntegerField()
+    index = models.IntegerField()
+
+class Meta:
+        constraints = [models.UniqueConstraint(name="performance_group",
+    fields=["group", "contest", "index"],
+)]
--- octet/settings.py
+++ octet/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     #'django.contrib.contenttypes',
     'django.contrib.staticfiles',
+    'app'
 ]
 
 MIDDLEWARE = [
